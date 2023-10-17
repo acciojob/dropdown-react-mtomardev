7d22c4f4248dc3e96b21fce2e8e00d72f068eb1b
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-10-17 20:57:58 +0530

    des

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const statesData = [{
 		}]
 	},{
 		name : "Bhopal",
-		description:"DBhopal is a city in the central Indian state of Madhya Pradesh. It's one of India’s greenest city. There are two main lakes, the Upper Lake and the Lower Lake.",
+		description:"DBhopal is a city in the central Indian state of Madhya Pradesh. It\'s one of India\'s greenest city. There are two main lakes, the Upper Lake and the Lower Lake.",
 		landmarks :[{
 			name : "MANIT",
 			description:"Maulana Azad National Institute of Technology Bhopal is a public technical university located in Bhopal, Madhya Pradesh, India. It is part of a group of publicly funded institutions in India known as National Institutes of Technology. ",
